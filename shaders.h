3a842b913d864f3fd580a16827f6ea78cd11229b
Ilya Lopatkin <ilopatkin@scottlogic.com>
2014-03-11 14:30:36 +0000

    removed unnecessary files

--- shaders.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHADERS_H
-#define SHADERS_H
-
-#include <GLES2/gl2.h>
-
-GLuint loadShader(GLenum type, const char *source);
-GLuint buildProgram(GLuint vertexShader, GLuint fragmentShader, const char * vertexPositionName);
-
-#endif
